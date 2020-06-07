f01e729c243e6ede42b18a37210196d63b8e7513
Pratik K <pkrockstar7@gmail.com>
2020-06-07 21:52:13 +0530

    [Addition] removed \`signup_route\`
    - signup_route not required (use CAS)
    - removed (temp) \`update_user_route\`

--- backend/app/user/controller.py
+++ backend/app/user/controller.py
@@ -13,12 +13,6 @@ from ..utils.token import verify_token
 router = APIRouter()
 
 
-@router.post("/signup", tags=["signup", "auth", "testing"])
-async def signup_route(user: User, db: AsyncIOMotorClient = Depends(get_database)):
-    """Signup route (for testing)"""
-    return {"success": True}
-
-
 @router.get("/{user_id}", response_model=UserInResponse, tags=["fetch"])
 async def get_user_route(user_id: ObjectID,
                          db: AsyncIOMotorClient = Depends(get_database)) -> UserInResponse:
@@ -27,12 +21,12 @@ async def get_user_route(user_id: ObjectID,
     return {"success": True}
 
 
-@router.patch("/", response_model=UserInResponse, tags=["update"])
-async def update_user_route(token: dict = Depends(verify_token),
-                            db: AsyncIOMotorClient = Depends(get_database)) -> UserInResponse:
-    """Update user information for logged in user"""
-    # TODO update user functionality
-    return {"success": True}
+# NOTE: not required as of now
+# @router.patch("/", response_model=UserInResponse, tags=["update"])
+# async def update_user_route(token: dict = Depends(verify_token),
+#                             db: AsyncIOMotorClient = Depends(get_database)) -> UserInResponse:
+#     """Update user information for logged in user"""
+#     return {"success": True}
 
 
 @router.post("/like/{category_id}/{item_id}", response_model=LikeInResponse, tags=["like"])
