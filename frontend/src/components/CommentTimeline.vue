a9d2e65c6cbdaf1623537d44c08b036b8b3fabd0
Abhijit Manatkar <abhijit.manatkar@students.iiit.ac.in>
2020-06-21 16:41:43 +0530

    [Add] Comment timeline component

--- frontend/src/components/CommentTimeline.vue
+++ frontend/src/components/CommentTimeline.vue
@@ -1,5 +1,22 @@
 <template>
-  
+  <div class="comment-timeline">
+  <v-timeline dense>
+    <v-timeline-item v-for="n in 3" :key="n">
+      <template v-slot:icon>
+        <v-avatar>
+          <img src="http://i.pravatar.cc/64">
+        </v-avatar>
+      </template>
+      <v-card class="elevation-2">
+        <v-card-title class="headline">Great!</v-card-title>
+        <v-card-subtitle>Commenter Name -  June 14, 10:30 am</v-card-subtitle>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at.
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+  </div>
 </template>
 
 <script>
@@ -7,3 +24,9 @@
     name: 'CommentTimeline',
   }
 </script>
+
+<style>
+  .comment-timeline {
+    width: 90%;
+  }
+</style>
