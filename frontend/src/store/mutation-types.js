aa7e630c9b6caac7fae25f3165bc2979fa54d2fe
gsc2001 <gurkirat.singh@students.iiit.ac.in>
2020-06-24 13:54:06 +0530

    [Add] mutation-types + README instruction

---
+++ frontend/src/store/mutation-types.js
@@ -0,0 +1,18 @@
+// items
+export const ADD_ITEM = "ADD_ITEM";
+export const DEL_ITEM = "DEL_ITEM";
+export const SET_LIKES = "SET_LIKES";
+export const SET_RATINGS = "SET_RATINGS";
+
+// likes
+export const INIT_LIKES = "INIT_LIKES";
+export const ADD_LIKE = "ADD_LIKE";
+export const DEL_LIKE = "DEL_LIKE";
+
+// ratings
+export const INIT_RATINGS = "INIT_RATINGS";
+export const ADD_RATING = "ADD_RATING";
+export const DEL_RATING = "DEL_RATING";
+
+// user
+export const ADD_USERDATA = "ADD_USERDATA";
