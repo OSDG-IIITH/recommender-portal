aeb396fc1fc1ec6088b3b9c5ac64eceb25f8b0b3
kush-garg <59833869+kush-garg@users.noreply.github.com>
2020-06-30 18:20:15 +0530

    [ADD] mutation: likes/ADD_LIKE (#70)

--- frontend/src/store/modules/likes.js
+++ frontend/src/store/modules/likes.js
@@ -22,8 +22,10 @@ const mutations = {
         }
         state.push(...likes_list);
     },
-    ADD_LIKE: (state, like) => {
-        // TODO: Add like to state
+    ADD_LIKE: (state, payload) => {
+        state.push({
+            _id: payload.item_id, 
+            value: payload.value})
     },
     DEL_LIKE: (state, payload) => {
         index = state.findIndex(likes => likes["_id"] == payload.item_id)
