a0e25e88665cd06f0e1fc945dde8d96036ae941c
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2020-06-30 18:51:34 +0530

    [Add] Intergration Template (#71)

--- frontend/src/components/FormCommon.vue
+++ frontend/src/components/FormCommon.vue
@@ -1,46 +1,54 @@
 <template>
-    <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-    >
+    <v-form v-model="valid">
+        <v-checkbox v-model="form.hidden" label="Hidden"></v-checkbox>
 
         <v-text-field
-          label="Flags"
-        ></v-text-field>
-
-        <v-checkbox
-          v-model="checkbox"
-          label="Hidden"
-        ></v-checkbox>
-
-        <v-text-field
-          v-model="name"
-          label="Title"
-          required
+            v-model="form.title"
+            type="text"
+            label="Title"
+            :rules="nameRules"
         ></v-text-field>
 
         <v-text-field
-          v-model="url"
-          label="Url"
-          required
+            v-model="form.url"
+            type="text"
+            label="Url"
+            :rules="nameRules"
         ></v-text-field>
 
+        <v-text-field v-model="form.flags" label="Flags"></v-text-field>
+        <small>Add values seperated by commas</small>
         <v-text-field
-          v-model="numberValue"
-          label="Year"
-          required
+            v-model="form.genres"
+            label="Genres"
+            type="text"
         ></v-text-field>
+        <small>Add values seperated by commas</small>
 
         <v-text-field
-          v-model="name"
-          label="Genre"
+            v-model="form.year_release"
+            type="text"
+            label="Year"
+            :rules="nameRules"
         ></v-text-field>
+
+        <v-btn
+            color="secondary"
+            @click="$emit('continue', JSON.stringify(form))"
+            :disabled="!valid"
+            >Continue</v-btn
+        >
+        <v-btn text @click="$emit('cancel')">Cancel</v-btn>
     </v-form>
 </template>
 
 <script>
 export default {
-    name: "FormCommon"
+    name: "FormCommon",
+    data: () => ({
+        valid: true,
+        nameRules: [v => !!v || "Required"],
+        form: {}
+    })
 };
 </script>
--- frontend/src/components/FormSpecific.vue
+++ frontend/src/components/FormSpecific.vue
@@ -1,138 +1,146 @@
 <template>
-<div>
-    <div v-if="sheetType === 'movies'">
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
-            <v-text-field
-                v-model="name"
-                label="Language"
-                required
-            ></v-text-field>
+    <div>
+        <div v-if="sheetType === 'movies'">
+            <v-form ref="form" v-model="valid">
+                <v-text-field
+                    v-model="form.language"
+                    label="Language"
+                    :rules="required"
+                ></v-text-field>
 
-            <v-text-field
-                v-model="name"
-                label="Director"
-                required
-            ></v-text-field>
+                <v-text-field
+                    v-model="form.director"
+                    label="Director"
+                    :rules="required"
+                ></v-text-field>
 
-            <v-text-field
-                v-model="name"
-                label="Streaming on"
-                required
-            ></v-text-field>
+                <v-text-field
+                    v-model="form.streaming"
+                    label="Streaming on"
+                    :rules="required"
+                ></v-text-field>
+                <v-btn
+                    color="secondary"
+                    @click="$emit('continue', JSON.stringify(form))"
+                    :disabled="!valid"
+                    >Submit</v-btn
+                >
+                <v-btn text @click="$emit('cancel')">Cancel</v-btn>
+            </v-form>
+        </div>
+        <div v-else-if="sheetType === 'anime'">
+            <v-form ref="form" v-model="valid">
+                <v-text-field
+                    v-model="form.seasons"
+                    label="Seasons"
+                    :rules="required"
+                ></v-text-field>
 
-        </v-form>
+                <v-text-field
+                    v-model="form.episode_length"
+                    label="Episode Length"
+                    :rules="required"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="form.season_length"
+                    label="Season Length"
+                    :rules="required"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="form.streaming"
+                    label="Streaming on"
+                    :rules="required"
+                ></v-text-field>
+                <v-btn
+                    color="secondary"
+                    @click="$emit('continue', JSON.stringify(form))"
+                    :disabled="!valid"
+                    >Submit</v-btn
+                >
+                <v-btn text @click="$emit('cancel')">Cancel</v-btn>
+            </v-form>
+        </div>
+        <div v-else-if="sheetType === 'shows'">
+            <v-form ref="form" v-model="valid">
+                <v-text-field
+                    v-model="form.seasons"
+                    label="Seasons"
+                    :rules="required"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="form.episode_length"
+                    label="Episode Length"
+                    :rules="required"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="form.season_length"
+                    label="Season Length"
+                    :rules="required"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="form.streaming"
+                    label="Streaming on"
+                    :rules="required"
+                ></v-text-field>
+                <v-btn
+                    color="secondary"
+                    @click="$emit('continue', JSON.stringify(form))"
+                    :disabled="!valid"
+                    >Submit</v-btn
+                >
+                <v-btn text @click="$emit('cancel')">Cancel</v-btn>
+            </v-form>
+        </div>
+        <div v-else-if="sheetType === 'music'">
+            <v-form ref="form" v-model="valid">
+                <v-text-field
+                    v-model="form.artist"
+                    label="Artist"
+                    :rules="required"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="form.album"
+                    label="Album"
+                    :rules="required"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="form.streaming"
+                    label="Streaming on"
+                    :rules="required"
+                ></v-text-field>
+                <v-btn
+                    color="secondary"
+                    @click="$emit('continue', JSON.stringify(form))"
+                    :disabled="!valid"
+                    >Submit</v-btn
+                >
+                <v-btn text @click="$emit('cancel')">Cancel</v-btn>
+            </v-form>
+        </div>
+        <div v-else-if="sheetType === 'books'">
+            <v-form ref="form" v-model="valid">
+                <v-text-field
+                    v-model="form.author"
+                    label="Author"
+                    :rules="required"
+                ></v-text-field>
+                <v-btn
+                    color="secondary"
+                    @click="$emit('continue', JSON.stringify(form))"
+                    :disabled="!valid"
+                    >Submit</v-btn
+                >
+                <v-btn text @click="$emit('cancel')">Cancel</v-btn>
+            </v-form>
+        </div>
     </div>
-    <div v-else-if="sheetType === 'anime'">
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
-            <v-text-field
-                v-model="numberValue"
-                label="Seasons"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Episode Length"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Season Length"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Streaming on"
-                required
-            ></v-text-field>
-
-        </v-form>
-    </div>
-    <div v-else-if="sheetType === 'shows'">
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
-            <v-text-field
-                v-model="numberValue"
-                label="Seasons"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Episode Length"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Season Length"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Streaming on"
-                required
-            ></v-text-field>
-
-        </v-form>
-    </div>
-    <div v-else-if="sheetType === 'music'">
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
-
-            <v-text-field
-                v-model="name"
-                label="Artist"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Album"
-                required
-            ></v-text-field>
-
-            <v-text-field
-                v-model="name"
-                label="Streaming on"
-                required
-            ></v-text-field>
-
-        </v-form>
-    </div>
-    <div v-else-if="sheetType === 'books'">
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
-
-            <v-text-field
-                v-model="name"
-                label="Author"
-                required
-            ></v-text-field>
-
-        </v-form>
-    </div>
-</div>
 </template>
 
 <script>
@@ -140,6 +148,11 @@ export default {
     name: "FormSpecific",
     props: {
         sheetType: String
-    }
+    },
+    data: () => ({
+        valid: true,
+        form: {},
+        required: [v => !!v || "Required"]
+    })
 };
 </script>
