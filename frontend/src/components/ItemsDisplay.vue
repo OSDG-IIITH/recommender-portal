84b4a6c7f75491f3d5322e14663cd659245dc631
gsc2001 <gurkirat.singh@students.iiit.ac.in>
2020-07-10 00:33:13 +0530

    [Fix] Items display on tab change

--- frontend/src/components/ItemsDisplay.vue
+++ frontend/src/components/ItemsDisplay.vue
@@ -1,45 +1,45 @@
 <template>
     <v-container fluid>
-            <v-row class="masonry" align="start" justify="space-between" dark>
-                <v-col
-                    class="child"
-                    cols="12"
-                    xs="12"
-                    sm="6"
-                    md="4"
-                    lg="3"
-                    xl="2"
-                    v-for="post_key in post_keys"
-                    :key="post_key"
-                >
-                    <Item :post_id="post_key" />
-                </v-col>
-            </v-row>
+        <v-row class="masonry" align="start" justify="space-between" dark>
+            <v-col
+                class="child"
+                cols="12"
+                xs="12"
+                sm="6"
+                md="4"
+                lg="3"
+                xl="2"
+                v-for="post_key in post_keys"
+                :key="post_key"
+            >
+                <Item :post_id="post_key" />
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
-import Item from '@/components/Item'
+import Item from "@/components/Item";
 
-var msnry = new Masonry('.masonry', {
-  // options
-  itemSelector: '.child'
-})
+var msnry = new Masonry(".masonry", {
+    // options
+    itemSelector: ".child"
+});
 
 export default {
-  name: 'ItemsDisplay',
-  components: {
-    Item
-  },
-  mounted: async function () {
-    await this.$store.dispatch('items/fetchItems', this.$route.name)
-  },
-  computed: {
-    post_keys: function () {
-      return this.$store.getters['items/getKeysByCategory'](
-        this.$route.name
-      )
+    name: "ItemsDisplay",
+    components: {
+        Item
+    },
+    created: async function() {
+        await this.$store.dispatch("items/fetchItems", this.$route.name);
+    },
+    computed: {
+        post_keys: function() {
+            return this.$store.getters["items/getKeysByCategory"](
+                this.$route.name
+            );
+        }
     }
-  }
-}
+};
 </script>
