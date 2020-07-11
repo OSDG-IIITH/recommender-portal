efff8a5d3054bccac172503a4194bd50a3a6a18c
Pratik K <pkrockstar7@gmail.com>
2020-07-12 05:12:26 +0530

    [Addition] skeleton loader for `ItemsDisplay` (Fixes #105)

--- frontend/src/components/ItemsDisplay.vue
+++ frontend/src/components/ItemsDisplay.vue
@@ -1,9 +1,22 @@
 <template>
     <v-container fluid>
-        <v-overlay :absolute="true" :value="loading">
-            <v-progress-circular indeterminate size="64"></v-progress-circular>
-        </v-overlay>
         <v-row class="masonry" align="start" justify="space-between" dark>
+        <template v-if="loading">
+            <v-col
+                class="child"
+                cols="12"
+                xs="12"
+                sm="6"
+                md="4"
+                lg="3"
+                xl="2"
+                v-for="_ in stubArray"
+                :key="_"
+            >
+                <v-boilerplate type="card-avatar, article, actions"></v-boilerplate>
+            </v-col>
+        </template>
+        <template v-else>
             <v-col
                 class="child"
                 cols="12"
@@ -17,46 +30,61 @@
             >
                 <Item :post_id="post_key" />
             </v-col>
+        </template>
         </v-row>
     </v-container>
 </template>
 
 <script>
-import Item from "@/components/Item";
+import Item from '@/components/Item'
 
-var msnry = new Masonry(".masonry", {
-    // options
-    itemSelector: ".child"
-});
+var msnry = new Masonry('.masonry', {
+  // options
+  itemSelector: '.child'
+})
 
 export default {
-    name: "ItemsDisplay",
-    components: {
-        Item
-    },
-    data() {
-        return {
-            loading: false
-        };
-    },
-    created: async function() {
-        this.loading = true;
-        await this.$store.dispatch("items/fetchItems", this.$route.name);
-        this.loading = false;
-    },
-    computed: {
-        post_keys: function() {
-            return this.$store.getters["items/getKeysByCategory"](
-                this.$route.name
-            );
-        }
-    },
-    watch: {
-        $route: async function(to, from) {
-            this.loading = true;
-            await this.$store.dispatch("items/fetchItems", to.name);
-            this.loading = false;
-        }
+  name: 'ItemsDisplay',
+  components: {
+    Item,
+    VBoilerplate: {
+      functional: true,
+      render (h, { data, props, children }) {
+        return h('v-skeleton-loader', {
+          ...data,
+          props: {
+            boilerplate: true,
+            elevation: 2,
+            ...props
+          }
+        }, children)
+      }
     }
-};
+  },
+  data () {
+    return {
+      loading: false,
+      stubArray: Array.from({ length: Math.floor(Math.random() * (10 - 6 + 1) + 6) })
+    }
+  },
+  created: async function () {
+    this.loading = true
+    await this.$store.dispatch('items/fetchItems', this.$route.name)
+    this.loading = false
+  },
+  computed: {
+    post_keys: function () {
+      return this.$store.getters['items/getKeysByCategory'](
+        this.$route.name
+      )
+    }
+  },
+  watch: {
+    $route: async function (to, from) {
+      this.loading = true
+      await this.$store.dispatch('items/fetchItems', to.name)
+      this.loading = false
+    }
+  }
+}
 </script>
