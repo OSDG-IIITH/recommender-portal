b552c4953ce79d2ead53d6d25f247a5fa7ff8af1
Pratik K <pkrockstar7@gmail.com>
2020-07-12 01:13:50 +0530

    [Update] UI changes to AddItem*

--- frontend/src/components/AddItemSheet.vue
+++ frontend/src/components/AddItemSheet.vue
@@ -1,32 +1,26 @@
 <template>
-    <v-stepper v-model="stepNum" vertical dark>
+    <v-stepper v-model="stepNum" vertical>
         <v-overlay :absolute="true" :value="overlay">
             <v-progress-circular indeterminate size="64"></v-progress-circular>
         </v-overlay>
-        <h3
-            class="pa-2 ma-1 text-center text-sm-left text-overline white--text"
-        >
+        <h3 class="pa-2 ma-1 text-center text-sm-left text-overline white--text">
             Add {{ sheetType }}
         </h3>
-        <v-stepper-step editable :complete="stepNum > 1" step="1">
-            Step 1
-            <small>Common details</small>
-        </v-stepper-step>
+        <v-stepper-step editable :complete="stepNum > 1" step="1"> Step 1 <small>Common details</small></v-stepper-step>
         <v-stepper-content step="1">
-            <!-- Common fields form -->
-            <ItemCommonForm v-on:continue="submit1" v-on:cancel="cancel" />
+            <ItemCommonForm
+              v-on:continue="commonSubmit"
+              v-on:next="goToSpecific"
+              v-on:cancel="cancel"
+            />
         </v-stepper-content>
 
-        <v-stepper-step step="2">
-            Step 2
-            <small>Extra details</small>
-        </v-stepper-step>
+        <v-stepper-step editable step="2">Step 2 <small>Extra details</small></v-stepper-step>
         <v-stepper-content step="2">
-            <!-- Actual schema depending on $route.name -->
-
             <ItemSpecificForm
                 :sheetType="sheetType"
-                v-on:continue="submit2"
+                v-on:continue="specificSubmit"
+                v-on:previous="goToCommon"
                 v-on:cancel="cancel"
             />
         </v-stepper-content>
@@ -34,81 +28,68 @@
 </template>
 
 <script>
-import ItemCommonForm from "@/components/ItemCommonForm";
-import ItemSpecificForm from "@/components/ItemSpecificForm";
+import ItemCommonForm from '@/components/ItemCommonForm'
+import ItemSpecificForm from '@/components/ItemSpecificForm'
 
 export default {
-    name: "AddPostSheetTemp",
-    components: {
-        ItemCommonForm,
-        ItemSpecificForm
-    },
-    props: {
-        stepNum: Number,
-        closeSheet: Function,
-        sheetType: String
-    },
-    data() {
-        return {
-            overlay: false,
-            formData: {}
-        };
-    },
-    methods: {
-        submit1: function(data) {
-            // get back data
-            this.formData = JSON.parse(data);
-
-            // convert flags to array
-            if (this.formData.flags) {
-                this.formData.flags = this.formData.flags
-                    .split(",")
-                    .map(a => a.trim());
-            }
-
-            // convert genres to array
-            if (this.formData.genres) {
-                this.formData.genres = this.formData.genres
-                    .split(",")
-                    .map(a => a.trim());
-            }
-
-            // normalize URL (add https://)
-            const url = this.formData.url;
-            if (!url.includes("http://") && !url.includes("https://"))
-                this.formData.url = "https://" + url;
-
-            this.stepNum = 2;
-        },
-
-        submit2: async function(data) {
-            this.formData = {
-                ...this.formData,
-                ...JSON.parse(data)
-            };
-            console.log(this.formData);
-
-            // start loading
-            this.overlay = true;
-
-            // call the action
-            await this.$store.dispatch("items/addItem", {
-                // so as to remove __ob__
-                ...JSON.parse(JSON.stringify(this.formData)),
-                category: this.sheetType
-            });
-
-            // stop loading
-            this.overlay = false;
-
-            // close the sheet
-            this.formData = {};
-            this.closeSheet();
-        },
-        cancel: function() {
-            this.formData = {};
-            this.closeSheet();
-        }
+  name: 'AddItemSheet',
+  components: {
+    ItemCommonForm,
+    ItemSpecificForm
+  },
+  props: {
+    stepNum: Number,
+    closeSheet: Function,
+    sheetType: String
+  },
+  data () {
+    return {
+      overlay: false,
+      formData: {}
     }
-};
+  },
+  methods: {
+    goToCommon: function (currentValid, changeSection = true) {
+      this.specificValid = currentValid
+      if (changeSection) this.stepNum = 1
+    },
+    goToSpecific: function (currentValid, changeSection = true) {
+      this.commonValid = currentValid
+      if (changeSection) this.stepNum = 2
+    },
+    commonSubmit: function (data) {
+      // get back data
+      this.formData = JSON.parse(data)
+    },
+
+    specificSubmit: async function (data) {
+      this.formData = {
+        ...this.formData,
+        ...JSON.parse(data)
+      }
+      console.log(this.formData)
+
+      // start loading
+      this.overlay = true
+
+      // call the action
+      await this.$store.dispatch('items/addItem', {
+        // so as to remove __ob__
+        ...JSON.parse(JSON.stringify(this.formData)),
+        category: this.sheetType
+      })
+
+      // stop loading
+      this.overlay = false
+
+      // close the sheet
+      this.formData = {}
+      this.closeSheet()
+    },
+    cancel: function () {
+      this.formData = {}
+      this.closeSheet()
+    }
+  }
+}
 </script>
--- frontend/src/components/ItemCommonForm.vue
+++ frontend/src/components/ItemCommonForm.vue
@@ -1,128 +1,198 @@
 <template>
-    <v-form>
-        <v-text-field
-            v-model.trim="$v.form.title.$model"
-            type="text"
-            label="Title"
-            :error-messages="titleErrors"
-        ></v-text-field>
-
-        <v-text-field
-            v-model.trim="$v.form.url.$model"
-            type="text"
-            label="Url"
-            :error-messages="urlErrors"
-        ></v-text-field>
-
-        <v-text-field v-model="form.flags" label="Flags"></v-text-field>
-        <small>Add values seperated by commas</small>
-        <v-text-field
-            v-model="form.genres"
-            label="Genres"
-            type="text"
-        ></v-text-field>
-        <small>Add values seperated by commas</small>
-
-        <v-text-field
-            v-model="$v.form.year_release.$model"
-            type="text"
-            label="Year"
-            :error-messages="year_releaseErrors"
-        ></v-text-field>
-
-        <v-btn color="secondary" @click="submit" :disabled="$v.form.$invalid"
-            >Continue</v-btn
-        >
-        <v-btn text @click="cancel">Cancel</v-btn>
+    <v-form ref="form" v-model="valid">
+        <v-container fluid>
+            <v-row align="start" justify="space-around">
+                <v-col md="12" cols="12">
+                    <v-text-field
+                        v-model.trim="$v.form.title.$model"
+                        type="text"
+                        label="Title"
+                        min=3
+                        max=100
+                        outlined
+                        filled
+                        clearable
+                        dense
+                        placeholder="Angrezi Medium"
+                        prepend-inner-icon="mdi-format-title"
+                        :error-messages="titleErrors"
+                        required
+                    ></v-text-field>
+                </v-col>
+                <v-col md="12" cols="12">
+                    <v-text-field
+                        v-model.trim="$v.form.link.$model"
+                        type="url"
+                        label="Link"
+                        outlined
+                        filled
+                        clearable
+                        dense
+                        placeholder="https://www.hotstar.com/in/movies/angrezi-medium/"
+                        prepend-inner-icon="mdi-link-variant-plus"
+                        :error-messages="urlErrors"
+                        required
+                        ></v-text-field>
+                </v-col>
+                <v-col md="12" cols="12">
+                        <v-slider
+                        v-model="$v.form.year_release.$model"
+                        class="align-center"
+                        thumb-label="always"
+                        label="Year released"
+                        min="1950"
+                        :max="(new Date()).getFullYear()"
+                        :value="(new Date()).getFullYear()"
+                        :error-messages="year_releaseErrors"
+                        required
+                        ></v-slider>
+                </v-col>
+                <v-col md="12" cols="12">
+                     <v-combobox
+                        v-model.trim="$v.form.genres.$model"
+                        :items="genre_options"
+                        :search-input.sync="search"
+                        hint="Add a genre if it does not already exist"
+                        label="Genre(s)"
+                        hide-selected
+                        multiple
+                        outlined
+                        filled
+                        clearable
+                        prepend-inner-icon="mdi-view-grid-plus-outline"
+                        small-chips
+                        :error-messages="genreErrors"
+                        required
+                        >
+                        <template v-slot:no-data>
+                            <v-list-item>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+                                </v-list-item-title>
+                            </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                    </v-combobox>
+                </v-col>
+            </v-row>
+            <v-row align="start" justify="space-between">
+                <v-col cols="12" xs="12" class="align-center">
+                  <v-btn-toggle rounded>
+                    <v-btn outlined large @click="$emit('next', !$v.form.$invalid)" :color="isValid? 'success': 'error'">
+                        <v-icon>mdi-chevron-double-down</v-icon>
+                    </v-btn>
+                    <v-btn outlined large :color="$v.form.$invalid? 'warning' : 'error'" @click="$v.form.$invalid? 'reset' : 'cancel'">
+                        <v-icon left>{{$v.form.$invalid? 'mdi-refresh' : 'mdi-close'}}</v-icon>
+                        {{$v.form.$invalid? 'Reset' : 'Cancel'}}
+                    </v-btn>
+                  </v-btn-toggle>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-form>
 </template>
 
 <script>
-import { validationMixin } from "vuelidate";
-import { required, numeric } from "vuelidate/lib/validators";
+import { validationMixin } from 'vuelidate'
+import { required, numeric, url } from 'vuelidate/lib/validators'
 
 export default {
-    mixins: [validationMixin],
-    name: "ItemCommonForm",
-    data: () => ({
-        valid: false,
-        nameRules: [v => !!v || "Required"],
-        form: {
-            title: "",
-            url: "",
-            genres: "",
-            year_release: "",
-            flags: ""
-        }
-    }),
-    validations: {
-        form: {
-            title: { required },
-            url: {
-                required,
-                isURL(val) {
-                    const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
-                    const regex = new RegExp(expression);
-                    if (val.match(regex)) return true;
-                    return false;
-                }
-            },
-            year_release: { required, numeric }
-        }
-    },
-    computed: {
-        titleErrors: function() {
-            const errors = [];
-            if (!this.$v.form.title.$dirty) return errors;
-            !this.$v.form.title.required && errors.push("Title is required");
-            return errors;
-        },
-        urlErrors: function() {
-            const errors = [];
-            if (!this.$v.form.url.$dirty) return errors;
-            !this.$v.form.url.required && errors.push("URL is required");
-            !this.$v.form.url.isURL && errors.push("Not a correct URL");
-            return errors;
-        },
-        year_releaseErrors: function() {
-            const errors = [];
-            if (!this.$v.form.year_release.$dirty) return errors;
-            !this.$v.form.year_release.required &&
-                errors.push("Year is required");
-            !this.$v.form.year_release.numeric &&
-                errors.push("Year should be a number");
-            return errors;
-        }
-    },
-    methods: {
-        submit: function() {
-            const formData = JSON.stringify(this.form);
-            // this.form = {
-            //     title: "",
-            //     url: "",
-            //     genres: "",
-            //     year_release: "",
-            //     flags: ""
-            // };
-            this.$v.$reset();
-            this.$emit("continue", formData);
-        },
-        cancel: function() {
-            this.form = {
-                title: "",
-                url: "",
-                genres: "",
-                year_release: "",
-                flags: ""
-            };
-            this.$v.$reset();
-            this.$emit("cancel");
-        }
-    },
-    mounted() {
-        this.$root.$on("ItemCommonForm", () => {
-            this.cancel();
-        });
+  mixins: [validationMixin],
+  name: 'ItemCommonForm',
+  data: () => ({
+    valid: false,
+    genre_options: ['Gaming', 'Programming', 'Vue', 'Vuetify'],
+    search: null,
+    nameRules: [v => !!v || 'Required'],
+    form: {
+      title: '',
+      link: '',
+      genres: [],
+      year_release: '',
+      flags: []
     }
-};
+  }),
+  validations: {
+    form: {
+      title: { required },
+      link: {
+        required,
+        url
+      },
+      genres: {required},
+      year_release: { required, numeric }
+    }
+  },
+  watch: {
+    isValid (val, oldVal) {
+      this.$emit('next', val, false)
+    }
+  },
+  computed: {
+    titleErrors: function () {
+      const errors = []
+      if (!this.$v.form.title.$dirty) return errors
+      !this.$v.form.title.required && errors.push('Title is required')
+      return errors
+    },
+    urlErrors: function () {
+      const errors = []
+      if (!this.$v.form.link.$dirty) return errors
+      !this.$v.form.link.required && errors.push('URL is required')
+      !this.$v.form.link.url && errors.push('Not a correct URL')
+      return errors
+    },
+    genreErrors: function () {
+      const errors = []
+      if (!this.$v.form.genres.$dirty) return errors
+      !this.$v.genres.required && errors.push('At least ONE genre is required!')
+      return errors
+    },
+    year_releaseErrors: function () {
+      const errors = []
+      if (!this.$v.form.year_release.$dirty) return errors
+      !this.$v.form.year_release.required &&
+                  errors.push('Year is required')
+      !this.$v.form.year_release.numeric &&
+                  errors.push('Year should be a number')
+      return errors
+    }
+  },
+  methods: {
+    submit: function () {
+      const formData = JSON.stringify(this.form)
+      this.$v.$reset()
+      this.$emit('continue', formData)
+    },
+    reset: function () {
+      this.$refs.form.reset()
+      this.form = {
+        title: '',
+        link: '',
+        genres: [],
+        year_release: '',
+        flags: []
+      }
+      this.$v.$reset()
+    },
+    cancel: function () {
+      this.form = {
+        title: '',
+        link: '',
+        genres: [],
+        year_release: '',
+        flags: []
+      }
+      this.$v.$reset()
+      this.$refs.form.reset()
+      this.$emit('cancel')
+    }
+  },
+  mounted () {
+    this.$root.$on('ItemCommonForm', () => {
+      this.cancel()
+    })
+  }
+}
 </script>
