e2c51ab1eda701daf61179c1225af8a394f1afef
Pratik K <pkrockstar7@gmail.com>
2020-07-14 02:29:03 +0530

    [Update] Login overlay UI change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,10 +4,14 @@
         <v-main>
             <v-content>
                 <v-overlay :value="overlay">
-                    <v-progress-circular
+                     <v-progress-circular
+                        color="#4e4d5c"
+                        size="100"
+                        width="10"
                         indeterminate
-                        size="64"
-                    ></v-progress-circular>
+                    >
+                        <v-icon class="mdi-spin mdi-48px" color="#4e4d5c">mdi-lock-reset</v-icon>
+                    </v-progress-circular>
                 </v-overlay>
                 <router-view />
                 <AddItem :currPage="$route.name" />
@@ -17,40 +21,40 @@
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
-import AddItem from "@/components/AddItem";
-import { mapState } from "vuex";
+import Navbar from '@/components/Navbar'
+import AddItem from '@/components/AddItem'
+import { mapState } from 'vuex'
 export default {
-    name: "App",
-    props: {
-        source: String
+  name: 'App',
+  props: {
+    source: String
+  },
+  components: {
+    Navbar,
+    AddItem
+  },
+  data: () => ({
+    drawer: null,
+    sheet: false,
+    stepNum: 1,
+    currPage: 'Home',
+    current: 'Home'
+  }),
+  computed: mapState({
+    overlay: state => state.loading
+  }),
+  methods: {
+    closeSheet: function () {
+      this.sheet = false
     },
-    components: {
-        Navbar,
-        AddItem
+    openSheet: function (sheetType) {
+      this.stepNum = 1
+      this.sheetType = sheetType
+      this.sheet = true
     },
-    data: () => ({
-        drawer: null,
-        sheet: false,
-        stepNum: 1,
-        currPage: "Home",
-        current: "Home"
-    }),
-    computed: mapState({
-        overlay: state => state.loading
-    }),
-    methods: {
-        closeSheet: function() {
-            this.sheet = false;
-        },
-        openSheet: function(sheetType) {
-            this.stepNum = 1;
-            this.sheetType = sheetType;
-            this.sheet = true;
-        },
-        changeCurrPage: function(newCurrPage) {
-            this.currPage = newCurrPage;
-        }
+    changeCurrPage: function (newCurrPage) {
+      this.currPage = newCurrPage
     }
-};
+  }
+}
 </script>
